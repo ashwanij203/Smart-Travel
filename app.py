7d4377f0e71b8adf7f7347b1510c4808b8d85b0a
Ashwani Jaiswal <143525462+ashwanij203@users.noreply.github.com>
2025-04-06 23:53:22 +0530

    app.py

--- app.py
+++ app.py
@@ -19,24 +19,41 @@ def is_travel_related(message):
 @app.route("/")
 def home():
     return render_template("Index.html")
-genai.configure(api_key="AIzaSyAuCfBJrx3gSWfeiWHp73I1wKg9wGF5g_Y")
+# genai.configure(api_key="AIzaSyAuCfBJrx3gSWfeiWHp73I1wKg9wGF5g_Y")
+# model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key="AIzaSyAzAPMnDiW1LmgC1gIMSyb2voY4zItrnzA")
 model = genai.GenerativeModel("gemini-1.5-flash")
+chat_session = model.start_chat(history=[
+{
+    "role": "user",
+    "parts": [
+        "You are an AI trained to act as a personal travel assistant. Only answer questions related to travel planning, destination suggestions, itinerary creation, budget tips, local experiences, and travel safety."
+    ]
+}
 
+])
 @app.route("/get-response", methods=["POST"])
 
 def get_response():
     data = request.get_json()
     user_message = data.get("message")
-    if not is_travel_related(user_message):
-        return jsonify({"reply": "I'm your travel assistant 🧳✨ Please ask travel-related questions only."})
-    else:
-        if not user_message:
-            return jsonify({"reply": "No message received"}), 400
+    #response = model.generate_content(user_message)
+    try:
+        response = chat_session.send_message(user_message)
+        return jsonify({"reply": response.text if response.text else "I'm here to support you through your exam stress!"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
-        try:
-            response = model.generate_content(user_message)
-            return jsonify({"reply": response.text})
-        except Exception as e:
-            return jsonify({"reply": "Error from Gemini API"}), 500
+    # if not is_travel_related(user_message):
+    #     return jsonify({"reply": "I'm your travel assistant 🧳✨ Please ask travel-related questions only."})
+    # else:
+    #     if not user_message:
+    #         return jsonify({"reply": "No message received"}), 400
+
+    #     try:
+          #  response = model.generate_content(user_message)
+          #  return jsonify({"reply": response.text})
+        # except Exception as e:
+        #     return jsonify({"reply": "Error from Gemini API"}), 500
 if __name__ == "__main__":
     app.run(debug=True)
